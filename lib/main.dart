e88a7a2a3c529284ce03325eca89f57ac45d19db
Muhammad Adnan11 <adnankhalil984@gmail.in>
2024-02-28 12:35:55 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,53 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: AppBar(
-        title: Center(child: Text('My second app')),
-      ),
-    )
+      home: Scaffold(
+        appBar: AppBar (
+          title: Text('my second app'),
+          ),
+          // body: Column(
+          //   mainAxisAlignment: MainAxisAlignment.center,
+          //   crossAxisAlignment: CrossAxisAlignment.center,
+          //   children: [
+          //     Expanded(
+          //     child: Container(
+          //       color: Colors.blue,
+          //       height: 10,
+          //       child: Center(child: Text('this is goog app')),
+          //    ),),
+          //  Expanded(
+          //   child: Container(
+          //       color: Colors.green,
+          //       height: 10,
+          //       child: Center(child: Text('this is bad app')),
+          //    ),
+          //  ),
+          //   ],
+          //   ),
+          body: Column(
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.blue,
+                  ),
+                  Container(
+                    height:100,
+                    width: 100,
+                    color: Colors.green,
+                  ),
+                  Container(
+                    height: 100,
+                    width: 100,
+                    color: Colors.black,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        )
+    );
   }
 }
